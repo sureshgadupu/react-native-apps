ad9364bd3f45daa2758ce4c764c39ba8d86a9d68
sureshgadupu <gskforums@yahoo.com>
2020-05-17 18:29:34 +1200

    adjusting padding based on OS and device

--- todo-app/src/views/Header.jsx
+++ todo-app/src/views/Header.jsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Platform, StatusBar } from "react-native";
 
 const Header = () => {
-	return (
-		<View style={styles.header}>
-			<Text style={styles.title}>My Todos</Text>
-		</View>
-	);
+  return (
+    <View style={styles.header}>
+      <Text style={styles.title}>My Todos</Text>
+    </View>
+  );
 };
 
 export default Header;
 
 const styles = StyleSheet.create({
-	header: {
-		// flex: 1,
-		// flexDirection: 'row',
-		height: 90,
-		paddingTop: 40,
-		backgroundColor: 'coral'
-		// marginTop: 50,
-		// justifyContent: 'center',
+  header: {
+    // flex: 1,
+    // flexDirection: 'row',
+    height: 70,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    backgroundColor: "coral",
+    // marginTop: 50,
+    // justifyContent: 'center',
 
-		// alignItems: 'stretch'
-	},
-	title: {
-		textAlign: 'center',
-		fontSize: 20,
-		color: 'white'
-	}
+    // alignItems: 'stretch'
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 20,
+    color: "white",
+  },
 });
