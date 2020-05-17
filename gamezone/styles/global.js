eca4920d4b03b427a513098a06e3ae73a079ad6d
sureshgadupu <gskforums@yahoo.com>
2020-05-17 18:14:44 +1200

    setting padding based on the os and device

--- gamezone/styles/global.js
+++ gamezone/styles/global.js
@@ -1,10 +1,10 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Platform, StatusBar } from "react-native";
 
 export const globalStyles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "white",
-    paddingTop: 20,
+    paddingTop: Platform.OS === "ios" ? 0 : StatusBar.currentHeight,
     margin: 5,
   },
   title: {
